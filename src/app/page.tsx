071a7383fd6e023745f822b4fa0fe96b726769af
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-10-25 19:41:25 +0000

    solo esta seccion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,8 +68,8 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900/50">
       <Header />
       <main className="flex-1">
-      <section className="w-full py-12 md:py-16 min-h-[85vh] flex items-center">
-          <div className="container mx-auto grid md:grid-cols-2 gap-8 h-full">
+      <section className="w-full py-12 md:py-16 min-h-[85vh] flex items-center px-8 md:px-12 lg:px-24">
+          <div className="mx-auto grid md:grid-cols-2 gap-8 h-full w-full">
             <div className="flex flex-col justify-center animate-slide-in-left">
               <div className="w-full">
                 <div>
@@ -94,7 +94,7 @@ export default function Home() {
               </div>
             </div>
             
-            <div className="relative flex items-center justify-center p-8">
+            <div className="relative flex items-center justify-center p-8 overflow-hidden">
               <div
                 className={cn(
                   'absolute inset-8 rounded-[2rem] transform -rotate-6 transition-colors duration-1000',
