ff1ec73c069917909311db360b02409edde51c8b
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-10-25 15:26:28 +0000

    no debe estas en una carpate para img para poder cambiarela

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,9 +1,9 @@
-import { SisiLogoHero } from '@/components/icons/sisi-logo-hero';
+import Image from 'next/image';
 
 export function Logo() {
   return (
     <div className="flex items-center gap-2 px-4 py-2 rounded-md">
-      <SisiLogoHero className="h-12 w-12" />
+      <Image src="/img/logo.svg" alt="SisiApp Logo" width={48} height={48} className="h-12 w-12" />
       <span className="font-bold text-xl text-white">SisiApp</span>
     </div>
   );
