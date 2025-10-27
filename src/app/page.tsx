3f20618d458728b1e5748ef7758fd1310062a657
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-10-27 23:55:09 +0000

    no volver tras hciste unb amierda lo anchaste yeso no fue loquet edije

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900/50">
       <Header />
       <main className="flex-1">
-      <section id="acerca-de" className="w-full md:pb-24 pt-16">
+      <section id="acerca-de" className="w-full md:pb-24 py-16">
           <div className="grid md:grid-cols-2 gap-12">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center animate-slide-in-left">
               <div>
@@ -80,7 +80,7 @@ export default function Home() {
                 </Button>
               </div>
             </div>
-            <div className="relative animate-slide-in-right overflow-hidden h-[500px] md:h-[700px]">
+            <div className="relative animate-slide-in-right overflow-hidden h-[500px] md:h-[700px] flex items-center">
               <div className="w-full h-full transform -rotate-6 rounded-[2rem] animate-change-bg-color" />
             </div>
           </div>
