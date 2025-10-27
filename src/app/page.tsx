1532842173638f1d623cacf5a8729a5b621e1f8e
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-10-27 23:31:55 +0000

    hacerlo mas pqeuno a ver si veo las esqueinas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
               </div>
             </div>
             <div className="relative animate-slide-in-right overflow-hidden h-[400px] md:h-[600px] flex items-center justify-center p-12">
-              <div className="w-full h-full transform -rotate-6 scale-125 rounded-[2rem] animate-change-bg-color" />
+              <div className="w-full h-full transform -rotate-6 rounded-[2rem] animate-change-bg-color" />
             </div>
           </div>
         </section>
@@ -198,5 +198,3 @@ export default function Home() {
     </div>
   );
 }
-
-    
