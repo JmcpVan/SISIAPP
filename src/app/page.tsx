209b0bd40699f7b4baa96680d9b1a6ec477f9942
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-10-28 00:00:54 +0000

    si no fue lo que te dije

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,8 +58,8 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <section id="acerca-de" className="w-full py-16 md:py-24">
-          <div className="grid md:grid-cols-2 gap-12">
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex flex-col justify-center animate-slide-in-left">
+          <div className="container mx-auto px-4 sm:px-6 lg:px-8 grid md:grid-cols-2 gap-12">
+            <div className="flex flex-col justify-center animate-slide-in-left">
               <div>
                 <h1 className="text-4xl md:text-5xl font-bold text-foreground tracking-tight">
                   SisiApp
@@ -80,7 +80,7 @@ export default function Home() {
                 </Button>
               </div>
             </div>
-            <div className="relative animate-slide-in-right overflow-hidden h-[500px] md:h-[700px] flex items-center">
+            <div className="relative animate-slide-in-right overflow-hidden h-[500px] flex items-center">
               <div className="w-full h-full transform -rotate-6 rounded-[2rem] animate-change-bg-color" />
             </div>
           </div>
