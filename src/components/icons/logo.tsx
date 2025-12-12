4f121c712c29e9f47b4374c5f68c148023ce9ede
Jovenes Incorruptibles <jovenesincorruptibles@gmail.com>
2025-12-12 19:27:04 +0000

    puede cambiar el lgo que esta por este

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <div className="flex items-center gap-2 px-4 py-2 rounded-md">
-      <Image src="/img/logo.svg" alt="SisiApp Logo" width={48} height={48} className="h-12 w-12" />
+      <Image src="/img/logo-circular.svg" alt="SisiApp Logo" width={48} height={48} className="h-12 w-12" />
       <span className="font-bold text-xl text-white">SisiApp</span>
     </div>
   );
